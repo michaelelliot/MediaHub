90a1c89d44bc4f7341abbfce0c2aebd54795ea0c
Michael Elliot <michael@digital.com.au>
2011-01-06 22:30:25 +0800

    Misc changes

---
+++ page_tags.php
@@ -0,0 +1,18 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var_dump($_COOKIE);
+
+?>
+<table cellpadding="0" cellspacing="0">
+    <tr>
+        <td>Title: </td><td><input id="title" type="text" value="" /></td>
+    </tr>
+    <tr>
+        <td>Year: </td><td><input id="title" type="text" value="" /></td>
+    <tr>
+        <td> : </td><td><input id="title" type="text" value="" /></td>
+    </tr>
+</table>
