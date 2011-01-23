f3bec26e6095fe18d7a6c32f4f89c66f88d21c9c
Michael Elliot <michael@digital.com.au>
2011-01-23 16:52:11 +0800

    Added error message

--- src/redirect.php
+++ src/redirect.php
@@ -1,17 +1,15 @@
 <?php
 session_start();
 require_once('config.inc.php');
+require_once('common.inc.php');
 require_once('TwitterOAuth/TwitterOAuth.php');
 
 # Build TwitterOAuth object and get temporary credentials
 $oauth = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
 $request_token = $oauth->getRequestToken(OAUTH_CALLBACK_URL);
 if (!$request_token || $oauth->http_code != 200) {
-    print_r($oauth);
-    print $oauth->http_code;
-print $oauth->http_header;
-exit;
-    header("Location: ./?twitter_auth_error");
+    add_message("There was an unexpected OAuth error while attempting to login with Twitter.");
+    header("Location: ./");
     exit;
 }
 # Save temporary credentials to session
